977f983d61c31301328e88c4f81d95f3a886a810
Keith <kalexander914@gmail.com>
2022-02-06 13:05:12 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,69 @@
+# Create List of students and grades
+keith = {
+    "name": "Ketih",
+    "homework": [90.0, 97.0, 75.0, 92.0],
+    "quizzes": [88.0, 40.0, 94.0],
+    "tests": [75.0, 90.0]
+}
+lauren = {
+    "name": "Lauren",
+    "homework": [100.0, 92.0, 98.0, 100.0],
+    "quizzes": [82.0, 83.0, 91.0],
+    "tests": [89.0, 97.0]
+}
+sam = {
+    "name": "Sam",
+    "homework": [0.0, 87.0, 75.0, 22.0],
+    "quizzes": [0.0, 75.0, 78.0],
+    "tests": [100.0, 100.0]
+}
+
+# print average grade of the students
+def average(numbers):
+    total = sum(numbers)
+    total = float(total)
+
+    return total / len(numbers)
+
+
+def get_average(student):
+    homework = average(student["homework"])
+    quizzes = average(student["quizzes"])
+    tests = average(student["tests"])
+
+    avg_sum = homework * 0.1 + quizzes * 0.3 + tests * 0.6
+    return avg_sum
+
+
+def get_letter_grade(score):
+    if score >= 90:
+        return "A"
+    elif score >= 80:
+        return "B"
+    elif score >= 70:
+        return "C"
+    elif score >= 60:
+        return "D"
+    else:
+        return "F"
+        return score
+
+
+# print get_letter_grade(get_average(lloyd))
+
+# print letter grade of class's average
+def get_class_average(class_list):
+    results = []
+
+    for student in class_list:
+        student_avg = get_average(student)
+        results.append(student_avg)
+    return average(results)  #
+
+
+students = [keith, lauren, sam]
+
+class_avg = get_class_average(students)
+print(class_avg)
+print(get_letter_grade(class_avg))
+
